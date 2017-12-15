362ad1a9665aca420ab30b16112153e133aa59f3
Fabian Steeg <steeg@hbz-nrw.de>
2017-12-15 16:24:20 +0100

    Don't show specific values of Raumsystematik
    See https://github.com/hbz/nwbib/issues/276

--- app/views/TableRow.java
+++ app/views/TableRow.java
@@ -32,28 +32,8 @@ public enum TableRow {
 		@Override
 		public String process(JsonNode doc, String property, String param,
 				String label, List<String> values, Optional<List<String>> keys) {
-			List<String> vs = values;
-			if (doc.has("spatial")) { // see https://github.com/hbz/nwbib/issues/276
-				List<String> remove = Arrays.asList(//
-						"http://purl.org/lobid/nwbib-spatial#n10",
-						"http://purl.org/lobid/nwbib-spatial#n12",
-						"http://purl.org/lobid/nwbib-spatial#n14",
-						"http://purl.org/lobid/nwbib-spatial#n24",
-						"http://purl.org/lobid/nwbib-spatial#n28",
-						"http://purl.org/lobid/nwbib-spatial#n35",
-						"http://purl.org/lobid/nwbib-spatial#n36",
-						"http://purl.org/lobid/nwbib-spatial#n37",
-						"http://purl.org/lobid/nwbib-spatial#n52",
-						"http://purl.org/lobid/nwbib-spatial#n54",
-						"http://purl.org/lobid/nwbib-spatial#n72",
-						"http://purl.org/lobid/nwbib-spatial#n74",
-						"http://purl.org/lobid/nwbib-spatial#n96",
-						"http://purl.org/lobid/nwbib-spatial#n97");
-				vs = vs.stream().filter(v -> !remove.contains(v))
-						.collect(Collectors.toList());
-			}
 			List<String> filtered =
-					vs.stream().filter(value -> !value.contains("http://dewey.info"))
+					values.stream().filter(value -> !value.contains("http://dewey.info"))
 							.collect(Collectors.toList());
 			return filtered.isEmpty() ? ""
 					: String.format("<tr><td>%s</td><td>%s</td></tr>", label,
