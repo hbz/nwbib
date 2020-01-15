875fe182c643d47d16fee49af13abdf9ac604115
Fabian Steeg <steeg@hbz-nrw.de>
2020-01-15 13:50:34 +0100

    Create import file to fix Kreis Mettmann
    See https://github.com/hbz/nwbib/issues/514

--- app/Import700n.java
+++ app/Import700n.java
@@ -1,6 +1,9 @@
 
 /* Copyright 2019 Fabian Steeg, hbz. Licensed under the GPLv2 */
 
+import static play.test.Helpers.running;
+import static play.test.Helpers.testServer;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -18,6 +21,7 @@ import org.apache.jena.ext.com.google.common.collect.Streams;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import controllers.nwbib.Lobid;
 import play.libs.Json;
 
 /**
@@ -42,32 +46,50 @@ public class Import700n {
 	 * @param args Optional, the input (jsonl) and the output (txt) file names
 	 */
 	public static void main(String[] args) {
-		if (args.length == 2) {
-			dataIn = new File(args[0]);
-			dataOut = new File(args[1]);
-		}
-		try (Scanner scanner = new Scanner(dataIn, StandardCharsets.UTF_8.name());
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-						new FileOutputStream(dataOut), StandardCharsets.UTF_8))) {
-			while (scanner.hasNextLine()) {
-				JsonNode record = Json.parse(scanner.nextLine());
-				Stream<String> subjects = Streams.concat(//
-						processSpatial(record), processSubject(record));
-				String resultLine = String.format("%s\t%s", //
-						record.get("hbzId").asText(),
-						subjects.collect(Collectors.joining(", "))//
-								// https://github.com/hbz/lobid-resources/issues/1018
-								.replaceAll("spatial#N05", "spatial#N04"));
-				resultLine = resultLine//
-						.replace("spatial#N04$$0", "Westfalen$$0")
-						.replace("Siebengebirge$$0https://nwbib.de/spatial#Q4236",
-								"Siebengebirge$$0https://nwbib.de/spatial#N23");
-				System.out.println(resultLine);
-				writer.write(resultLine + "\n");
+		running(testServer(3333), () -> {
+			if (args.length == 2) {
+				dataIn = new File(args[0]);
+				dataOut = new File(args[1]);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			try (Scanner scanner = new Scanner(dataIn, StandardCharsets.UTF_8.name());
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream(dataOut), StandardCharsets.UTF_8))) {
+				while (scanner.hasNextLine()) {
+					JsonNode record = Json.parse(scanner.nextLine());
+					// String resultLine = processLobidResource(record);
+					String resultLine = processNwbibSnapshot(record);
+					System.out.println(resultLine);
+					writer.write(resultLine + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private static String processNwbibSnapshot(JsonNode record) {
+		// See https://github.com/hbz/nwbib/issues/514
+		String id = record.get("hbzId").asText();
+		String result = processLobidResource(Lobid.getResource(id));
+		result = result.replace(
+				"Kreis Düsseldorf-Mettmann$$0https://nwbib.de/spatial#Q1787267",
+				"Kreis Mettmann$$0https://nwbib.de/spatial#Q6257");
+		return result;
+	}
+
+	private static String processLobidResource(JsonNode record) {
+		Stream<String> subjects = Streams.concat(//
+				processSpatial(record), processSubject(record));
+		String resultLine = String.format("%s\t%s", //
+				record.get("hbzId").asText(),
+				subjects.collect(Collectors.joining(", "))//
+						// https://github.com/hbz/lobid-resources/issues/1018
+						.replaceAll("spatial#N05", "spatial#N04"));
+		resultLine = resultLine//
+				.replace("spatial#N04$$0", "Westfalen$$0")
+				.replace("Siebengebirge$$0https://nwbib.de/spatial#Q4236",
+						"Siebengebirge$$0https://nwbib.de/spatial#N23");
+		return resultLine;
 	}
 
 	private static Stream<String> processSubject(JsonNode record) {
--- conf/700n-import.sh
+++ conf/700n-import.sh
@@ -1,4 +1,4 @@
-curl --header "Accept-Encoding: gzip" "http://lobid.org/resources/search?q=+inCollection.id%3A%22http%3A%2F%2Flobid.org%2Fresources%2FHT014176012%23%21%22+AND+NOT+inCollection.id%3A%22http%3A%2F%2Flobid.org%2Fresources%2FHT014846970%23%21%22+AND+%28_exists_%3Aspatial+OR+subject.source.id%3A%22https%3A%2F%2Fnwbib.de%2Fsubjects%22%29&format=jsonl" > 700n-import.jsonl.gz
+curl --header "Accept-Encoding: gzip" "http://nwbibsnapshot.lobid.org/resources/search?q=Kreis+AND+Mettmann+AND+NOT+D%C3%BCsseldorf&format=jsonl" > 700n-import.jsonl.gz
 gunzip 700n-import.jsonl.gz
 head 700n-import.jsonl > 700n-import-test.jsonl
 
