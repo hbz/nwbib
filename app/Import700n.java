ff1c0d74e3ad8278475913c9ac647758fc264f02
Fabian Steeg <steeg@hbz-nrw.de>
2020-01-21 14:59:46 +0100

    Create import file without N96 and N97
    See https://github.com/hbz/nwbib/issues/523

--- app/Import700n.java
+++ app/Import700n.java
@@ -83,8 +83,7 @@ public class Import700n {
 		Stream<String> subjects = Streams.concat(//
 				processSpatial(record), processSubject(record));
 		String resultLine = String.format("%s\t%s", //
-				record.get("hbzId").asText(),
-				subjects.collect(Collectors.joining(", "))//
+				record.get("hbzId").asText(), subjects.collect(Collectors.joining(", "))//
 						// https://github.com/hbz/lobid-resources/issues/1018
 						.replaceAll("spatial#N05", "spatial#N04"));
 		resultLine = resultLine//
@@ -100,17 +99,24 @@ public class Import700n {
 	private static Stream<String> processSubject(JsonNode record) {
 		Stream<JsonNode> subjects = asStream(record.findValue("subject"));
 		return subjects == null ? Collections.<String> emptyList().stream()
-				: subjects.filter(subject -> subject.has("source")
-						&& subject.get("source").get("id").textValue()
-								.startsWith("https://nwbib.de")
-						&& subject.has("id") && subject.has("label"))
+				: subjects
+						.filter(subject -> subject.has("source")
+								&& subject.get("source").get("id").textValue()
+										.startsWith("https://nwbib.de")
+								&& subject.has("id") && subject.has("label")
+								// https://github.com/hbz/nwbib/issues/523
+								&& (!subject.get("id").textValue().endsWith("N96")
+										&& !subject.get("id").textValue().endsWith("N97")))
 						.map(toAlephImportValue);
 	}
 
 	private static Stream<String> processSpatial(JsonNode record) {
 		Stream<JsonNode> spatials = asStream(record.findValue("spatial"));
 		return spatials == null ? Collections.<String> emptyList().stream()
-				: spatials.filter(spatial -> spatial.has("id") && spatial.has("label"))
+				: spatials.filter(spatial -> spatial.has("id") && spatial.has("label")
+				// https://github.com/hbz/nwbib/issues/523
+						&& (!spatial.get("id").textValue().endsWith("N96")
+								&& !spatial.get("id").textValue().endsWith("N97")))
 						.map(toAlephImportValue);
 	}
 
