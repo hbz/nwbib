cc9963d85889c4f8b1a709b984a6ccd3e11c97ea
Fabian Steeg <steeg@hbz-nrw.de>
2020-04-02 11:32:56 +0200

    Update lobid-resources query to exclude ZDB entries
    See https://github.com/hbz/nwbib/issues/540

--- conf/700n-import.sh
+++ conf/700n-import.sh
@@ -1,4 +1,4 @@
-curl --header "Accept-Encoding: gzip" "http://lobid.org/resources/search?q=spatial.id%3A*&format=jsonl" > 700n-import.jsonl.gz
+curl --header "Accept-Encoding: gzip" "http://lobid.org/resources/search?q=spatial.id%3A*+AND+NOT+inCollection.id%3A%22http%3A%2F%2Flobid.org%2Fresources%2FHT014846970%23%21%22&format=jsonl" > 700n-import.jsonl.gz
 gunzip 700n-import.jsonl.gz
 head -n 1000 700n-import.jsonl > 700n-import-test.jsonl
 cat 700n-import.jsonl | grep HT016542897 >> 700n-import-test.jsonl
