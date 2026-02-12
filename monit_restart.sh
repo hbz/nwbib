f3c120cb23c756917cfb44c2419c39d37c7e1e3f
Phu Tu <phu.tu@hbz-nrw.de>
2026-02-12 15:31:23 +0100

    Set JAVA_HOME, replace activator with sbt

--- monit_restart.sh
+++ monit_restart.sh
@@ -25,6 +25,7 @@ HOME="/home/sol"
 
 # it is important to set the proper locale
 . $HOME/.locale
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 JAVA_OPTS=$(echo "$JAVA_OPTS" |sed 's#,#\ #g')
 
 cd $HOME/git/$REPO
@@ -34,7 +35,9 @@ case $ACTION in
                     kill $(cat target/universal/stage/RUNNING_PID)
                     rm target/universal/stage/RUNNING_PID
                 fi
-		JAVA_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError" $HOME/activator-dist-1.3.5/activator "start $PORT"
+		sbt clean
+		sbt --java-home $JAVA_HOME stage
+		JAVA_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError" ./target/universal/stage/bin/nwbib -Dhttp.port=$PORT -no-version-check > monit_start.log &
 		;;
 	stop)
 		kill $(cat target/universal/stage/RUNNING_PID)
